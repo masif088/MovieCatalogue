f09eb6acb2b2e69b5259dc4000c08721c6948d1b
miqdadyyy <miqdad.farcha@gmail.com>
2018-10-14 22:28:52 +0700

    add Daily Reminder

---
+++ app/src/main/java/com/himasif/myf/moviecatalogue/DailyNotificationReceiver.java
@@ -0,0 +1,83 @@
+package com.himasif.myf.moviecatalogue;
+
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import java.util.Calendar;
+
+import static android.support.constraint.Constraints.TAG;
+
+/**
+ * Created by Dul mu'in on 14/10/2018.
+ */
+
+public class DailyNotificationReceiver extends BroadcastReceiver {
+
+    private static final int NOTIFICATION_ID = 911;
+
+    public DailyNotificationReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String title = context.getString(R.string.app_name);
+        String text = context.getString(R.string.daily_reminder_text);
+        showAlarmNotification(context, title, text, NOTIFICATION_ID);
+        Log.d(TAG, "onReceive : Notification : called");
+    }
+
+    private void showAlarmNotification(Context context, String title, String message, int notifId) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(
+                Context.NOTIFICATION_SERVICE);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, notifId, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri alarmRingtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_favorite_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setSound(alarmRingtone);
+        Log.d(TAG, "showAlarmNotification: Notification : called");
+        notificationManager.notify(notifId, builder.build());
+    }
+
+    public void setRepeatingAlarm(Context context) {
+        Log.d(TAG, "setRepeatingAlarm: Alarm Daily Setted");
+        cancelAlarm(context);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 7);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        Intent intent = new Intent(context, DailyNotificationReceiver.class);
+        PendingIntent pendingIntent =  PendingIntent.getBroadcast(context, NOTIFICATION_ID, intent, 0);
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+    }
+
+    public void cancelAlarm(Context context) {
+        Log.d(TAG, "cancelAlarm: Alarm daily Canceled");
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, DailyNotificationReceiver.class);
+        PendingIntent pendingIntent =  PendingIntent.getBroadcast(context, NOTIFICATION_ID, intent, 0);
+        alarmManager.cancel(pendingIntent);
+    }
+}
--- app/src/main/java/com/himasif/myf/moviecatalogue/Fragments/SettingsFragment.java
+++ app/src/main/java/com/himasif/myf/moviecatalogue/Fragments/SettingsFragment.java
@@ -2,25 +2,33 @@ package com.himasif.myf.moviecatalogue.Fragments;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.SwitchPreferenceCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.himasif.myf.moviecatalogue.DailyNotificationReceiver;
+import com.himasif.myf.moviecatalogue.MainActivity;
 import com.himasif.myf.moviecatalogue.R;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat{
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -32,6 +40,23 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         super.onResume();
 
         SwitchPreference dailyReminderSwitch = (SwitchPreference) findPreference(getResources().getString(R.string.pref_switch_daily_reminder));
+        String summary = "";
+        dailyReminderSwitch.setOnPreferenceChangeListener(new android.support.v7.preference.Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(android.support.v7.preference.Preference preference, Object newValue) {
+                SharedPreferences dailyPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+                boolean isActive = !dailyPreferences.getBoolean(getString(R.string.pref_switch_daily_reminder), true);
+                DailyNotificationReceiver dailyNotificationReceiver = new DailyNotificationReceiver();
+
+                if(isActive){
+                    dailyNotificationReceiver.setRepeatingAlarm(getContext());
+                } else {
+                    dailyNotificationReceiver.cancelAlarm(getContext());
+                }
+                Log.d(TAG, "onPreferenceChange: Status : " + isActive);
+                return true;
+            }
+        });
 
         SwitchPreference releaseReminderSwitch = (SwitchPreference) findPreference(getResources().getString(R.string.pref_switch_release_reminder));
 
